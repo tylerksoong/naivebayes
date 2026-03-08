ca039bbf6978b54408427188e045c8e5f1ca4b82
tylerksoong <soongkaisheng@gmail.com>
2026-03-07 18:07:39 -0800

    finsih bayes implemention

--- naive_bayes.py
+++ naive_bayes.py
@@ -3,32 +3,42 @@ from numpy.typing import ArrayLike
 import pandas as pd
 import email
 
+class NaiveBayesBernoulli:
+    def NaiveBayesBernoulli(self):
+        self.fj
+        self.pij
 
-def get_fj(X: ArrayLike, label_idx) -> ArrayLike:
-    #since we know that are classes are binary, will simply use parameterized version instead of ECDF
-    # mle estimator for Bernoulli p is simply the average
-    df = pd.DataFrame(X)
-    label_name = df.columns[label_idx]
-    average_given_label = df.groupby(label_name).mean()
-    return average_given_label.to_numpy()
+    def get_fj(self, X: ArrayLike, y) -> ArrayLike:
+        #since we know that are classes are binary, will simply use parameterized version instead of ECDF
+        # mle estimator for Bernoulli p is simply the average
+        df = pd.DataFrame(X)
+        average_given_label = df.groupby(y).mean()
+        return average_given_label.to_numpy()
+
+    def get_pij(self, y) -> dict:
+        probabilites = pd.Series(y).value_counts(normalize=True).to_dict()
+        return probabilites
+
+    def bernoulli_pdf(self, p, x):
+        return p if x == 1 else 1 - p
+
+    def fj_x(self, j, x: list):
+        retval = 1
+        for i, p in enumerate(self.fj[j]):
+            retval *= self.bernoulli_pdf(p, x[i])
+        return retval
+
+    def fit(self, X: ArrayLike, y):
+        self.fj = self.get_fj(X, y)
+        self.pij = self.get_pij(y)
 
 
+    def predict(self, x):
+        x = np.array(x)
+        numerator = self.pij[1] * self.fj_x(1, x)
 
-
-def get_pij(X: ArrayLike, label_idx) -> dict:
-    df = pd.DataFrame(X)
-    probabilites = df.iloc[:,label_idx].value_counts(normalize=True).to_dict()
-    return probabilites
-
-def bernoulli_pdf(p, x):
-    return p if x == 1 else 1 - p
-
-def get_probofx(fj, x: list, j):
-    nparr = np.array(x)
-    retval = 1
-    for i, p in enumerate(fj[j]):
-        retval *= bernoulli_pdf(p, x[i])
-    return retval
-
-
-y
+        denom = 0
+        for j in [0, 1]:
+            denom += self.pij[j] * self.fj_x(j, x)
+        
+        return numerator/denom
